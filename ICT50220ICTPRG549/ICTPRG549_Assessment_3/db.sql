449d94d934f054c0a639d04dd3b7fdc09ac393f3
ミア <S1554654@student.mp.edu.au>
2024-05-14 09:04:54 +0800

    lba

--- ICT50220ICTPRG549/ICTPRG549_Assessment_3/db.sql
+++ ICT50220ICTPRG549/ICTPRG549_Assessment_3/db.sql
@@ -6,8 +6,8 @@ use `wyz_mp_park`;
 drop table if exists `wyz_student`;
 create table `wyz_student`
 (
-  `wyz_id` char(8) NOT NULL primary key,
-  `wyz_std_name` varchar(20) not null
+  `wyz_stud_id` char(8) NOT NULL primary key,
+  `wyz_name` varchar(20) not null
 );
 
 drop table if exists `wyz_car`;
@@ -15,9 +15,13 @@ create table `wyz_car`
 (
   `wyz_rego_number` varchar(20) NOT NULL primary key,
   `wyz_owner` char(8) NOT NULL,
-  CONSTRAINT `fk1_car` FOREIGN KEY(`wyz_owner`) REFERENCES `wyz_student` (`wyz_id`)
+  CONSTRAINT `fk1_car` FOREIGN KEY(`wyz_owner`) REFERENCES `wyz_student` (`wyz_stud_id`)
 );
 
-insert into `wyz_student` values ("s7794540", "wyz_1");
-insert into `wyz_student` values ("s6487709", "wyz_2");
-insert into `wyz_student` values ("s8431155", "wyz_3");
+insert into `wyz_student` values ("s8717386", "wyz_1");
+insert into `wyz_student` values ("s9466656", "wyz_2");
+insert into `wyz_student` values ("s4536889", "wyz_3");
+insert into `wyz_student` values ("s2970066", "wyz_4");
+insert into `wyz_student` values ("s5685118", "wyz_5");
+insert into `wyz_student` values ("s3138609", "wyz_6");
+insert into `wyz_student` values ("s2778489", "wyz_7");
--- ICT50220ICTPRG549/ICTPRG549_Assessment_3/main.py
+++ ICT50220ICTPRG549/ICTPRG549_Assessment_3/main.py
@@ -6,7 +6,7 @@ import pymysql
 
 
 # Locomotive number verification
-def check_rn(wyz_rego_number):
+def check_rego_number(wyz_rego_number):
     if re.match("^c[0-9]{7}$", wyz_rego_number, re.ASCII):
         return True
     else:
@@ -14,35 +14,50 @@ def check_rn(wyz_rego_number):
 
 # for the vehicle system
 class Car:
-    def __init__(self, wyz_rego_number, wyz_std_obj):
+    def __init__(self, wyz_rego_number, wyz_stud_object):
         try:
-            assert check_rn(wyz_rego_number)
+            assert check_rego_number(wyz_rego_number)
             self.wyz_rego_number = wyz_rego_number
-            self.wyz_owner = wyz_std_obj
+            self.wyz_owner = wyz_stud_object
         except AssertionError:
             tkinter.messagebox.showerror("error", "Incorrect vehicle registration code")
             raise
 
 
 # studentsIDdetection
-def check_sid(wyz_id):
-    if re.match("^s[0-9]{7}$", wyz_id, re.ASCII):
+def check_sid(wyz_stud_id):
+    if re.match("^s[0-9]{7}$", wyz_stud_id, re.ASCII):
         return True
     else:
         return False
 
 # studentsclass
 class Student:
-    def __init__(self, wyz_std_name, wyz_id):
+    def __init__(self, wyz_name, wyz_stud_id):
         try:
-            self.wyz_std_name = wyz_std_name
-            assert check_sid(wyz_id)
-            self.wyz_id = wyz_id
+            self.wyz_name = wyz_name
+            assert check_sid(wyz_stud_id)
+            self.wyz_stud_id = wyz_stud_id
         except AssertionError:
             tkinter.messagebox.showerror("error", "The student number format is incorrect")
             raise
             
             
+# studentsmanagementclass
+class StudentManager:
+    def __init__(self):
+        self.wyz_std_list = []
+
+    def std_register(self, wyz_stud_object):
+        for s in self.wyz_std_list:
+            if s.wyz_stud_id == wyz_stud_object.wyz_stud_id:
+                tkinter.messagebox.showerror("error", "studentsalready exists")
+                return False
+        else:
+            self.wyz_std_list.append(wyz_stud_object)
+            return True
+
+
 # Vehicle management category
 class CarManager:
     def __init__(self):
@@ -58,33 +73,13 @@ class CarManager:
             return True
 
 
-# studentsmanagementclass
-class StudentManager:
-    def __init__(self):
-        self.wyz_s_list = []
-
-    def s_register(self, wyz_std_obj):
-        for s in self.wyz_s_list:
-            if s.wyz_id == wyz_std_obj.wyz_id:
-                tkinter.messagebox.showerror("error", "studentsalready exists")
-                return False
-        else:
-            self.wyz_s_list.append(wyz_std_obj)
-            return True
-
-
-# according tostudentsIDsort
-def sort_id(wyz_s_list, reverse=False):
-    return sorted(wyz_s_list, key=lambda x: x.get("wyz_id"), reverse=reverse)
-    
-    
 # according tostudentsIDsearch
-def search_studByID(wyz_id):
+def search_byID(wyz_stud_id):
     wyz_connect = pymysql.connect(host="localhost", user="root", password="", database="wyz_mp_park",
                                   charset="utf8")
     wyz_cursor = wyz_connect.cursor(cursor=pymysql.cursors.DictCursor)
-    wyz_sql = "select * from `wyz_student` where wyz_id = %s"
-    wyz_cursor.execute(wyz_sql, [wyz_id])
+    wyz_sql = "select * from `wyz_student` where wyz_stud_id = %s"
+    wyz_cursor.execute(wyz_sql, [wyz_stud_id])
     wyz_find = wyz_cursor.fetchone()
     if wyz_find is None:    
         return False
@@ -92,6 +87,12 @@ def search_studByID(wyz_id):
         return wyz_find
    
         
+# according tostudentsIDsort
+def sort_stdByID(wyz_std_list, reverse=False):
+    wyz_std_list.sort(key=lambda x: x.get("wyz_stud_id"), reverse=reverse)
+    return wyz_std_list
+    
+    
 # main Database connection andstudentsdata query
 wyz_connect = pymysql.connect(host="localhost", user="root", password="", database="wyz_mp_park", charset="utf8")
 wyz_cursor = wyz_connect.cursor(cursor=pymysql.cursors.DictCursor)
@@ -100,7 +101,7 @@ wyz_cursor.execute(wyz_sql)
 wyz_students = wyz_cursor.fetchall()
 
 # according tostudentsIDsortRegisteredstudentsinformation
-print(sort_id(wyz_students))
+print(sort_stdByID(wyz_students))
 
 
 # Register to respond to events
@@ -125,14 +126,14 @@ def register():
         tkinter.messagebox.showerror("error", "Vehicle number does not meet requirements")
         return False
     
-    wyz_find_student = search_studByID(wyz_id)
-    if wyz_find_student and wyz_find_student.get("wyz_std_name") == wyz_name:
+    wyz_find_student = search_byID(wyz_id)
+    if wyz_find_student and wyz_find_student.get("wyz_name") == wyz_name:
         wyz_sql = "select * from `wyz_car` where wyz_rego_number = %s"
         wyz_cursor.execute(wyz_sql, [wyz_rego_number])
         wyz_find_car = wyz_cursor.fetchone()
         if wyz_find_car is None:
             wyz_sql = "insert into `wyz_car` values (%s, %s)"
-            wyz_cursor.execute(wyz_sql, [wyz_rego_number, wyz_find_student.get("wyz_id")])
+            wyz_cursor.execute(wyz_sql, [wyz_rego_number, wyz_find_student.get("wyz_stud_id")])
             wyz_connect.commit()
             tkinter.messagebox.showinfo("Registration is successful", "vehicleRegistration is successful")
             return True
@@ -140,7 +141,7 @@ def register():
             tkinter.messagebox.showerror("error", "Vehicle has been registered！")
             return False
 
-    elif wyz_find_student and wyz_find_student.get("wyz_std_name") != wyz_name:
+    elif wyz_find_student and wyz_find_student.get("wyz_name") != wyz_name:
         tkinter.messagebox.showerror("error", "studentsnumber andstudentsName mismatch")
         return False
     else:
@@ -165,25 +166,25 @@ wyz_root = tkinter.Tk()
 wyz_root.title("wyz_Car_Registration")
 wyz_root.resizable(width=False, height=False)
 
-wyz_lb1 = tkinter.Label(wyz_root, text="wyz_Car_Registration", bg="coral", fg="darkmagenta", font=("Bold", 30))
+wyz_lb1 = tkinter.Label(wyz_root, text="wyz_Car_Registration", bg="cornsilk", fg="cornflowerblue", font=("Bold", 30))
 wyz_lb1.grid(row=0, column=0, columnspan=4, pady=10, ipadx=10, ipady=10)
 
-wyz_lb2 = tkinter.Label(wyz_root, text="wyz_Student Name", bg="papayawhip", fg="palegoldenrod", font=("Bold", 15))
+wyz_lb2 = tkinter.Label(wyz_root, text="wyz_Student Name", bg="mediumblue", fg="slategray", font=("Bold", 15))
 wyz_lb2.grid(row=1, column=1, padx=10, pady=10, ipadx=5, ipady=5)
 wyz_e1 = tkinter.Entry(wyz_root, font=("normal", 13))
 wyz_e1.grid(row=1, column=2, pady=10, ipadx=5, ipady=5)
 
-wyz_lb3 = tkinter.Label(wyz_root, text="wyz_Student ID", bg="papayawhip", fg="palegoldenrod", font=("Bold", 15))
+wyz_lb3 = tkinter.Label(wyz_root, text="wyz_Student ID", bg="mediumblue", fg="slategray", font=("Bold", 15))
 wyz_lb3.grid(row=2, column=1, padx=10, pady=10, ipadx=5, ipady=5)
 wyz_e2 = tkinter.Entry(wyz_root, font=("normal", 13))
 wyz_e2.grid(row=2, column=2, pady=10, ipadx=5, ipady=5)
 
-wyz_lb4 = tkinter.Label(wyz_root, text="wyz_Rego Number", bg="papayawhip", fg="palegoldenrod", font=("Bold", 15))
+wyz_lb4 = tkinter.Label(wyz_root, text="wyz_Rego Number", bg="mediumblue", fg="slategray", font=("Bold", 15))
 wyz_lb4.grid(row=3, column=1, padx=10, pady=10, ipadx=5, ipady=5)
 wyz_e3 = tkinter.Entry(wyz_root, font=("normal", 13))
 wyz_e3.grid(row=3, column=2, pady=10, ipadx=5, ipady=5)
 
-wyz_btn = tkinter.Button(wyz_root, text="wyz_Register", command=register, bg="yellowgreen", fg="tomato", font=("normal", 15))
+wyz_btn = tkinter.Button(wyz_root, text="wyz_Register", command=register, bg="lightcyan", fg="pink", font=("normal", 15))
 wyz_btn.grid(row=4, column=1, columnspan=2, pady=10, ipadx=5, ipady=5)
 wyz_root.mainloop()
 
